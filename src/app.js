a493b3da01d6328d1dd881779b05e454bc759e1e
dlkulp <damiankulp@gmail.com>
2016-05-21 14:55:10 -0700

    fixed linting issues

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const serveStatic = require('feathers').static;
-const favicon = require('serve-favicon');
-const compress = require('compression');
-const cors = require('cors');
-const feathers = require('feathers');
-const configuration = require('feathers-configuration');
-const hooks = require('feathers-hooks');
-const rest = require('feathers-rest');
-const bodyParser = require('body-parser');
-const socketio = require('feathers-socketio');
-const middleware = require('./middleware');
-const services = require('./services');
+const path = require("path");
+const serveStatic = require("feathers").static;
+const favicon = require("serve-favicon");
+const compress = require("compression");
+const cors = require("cors");
+const feathers = require("feathers");
+const configuration = require("feathers-configuration");
+const hooks = require("feathers-hooks");
+const rest = require("feathers-rest");
+const bodyParser = require("body-parser");
+const socketio = require("feathers-socketio");
+const middleware = require("./middleware");
+const services = require("./services");
 
 const app = feathers();
 
-app.configure(configuration(path.join(__dirname, '..')));
+app.configure(configuration(path.join(__dirname, "..")));
 
 app.use(compress())
-	.options('*', cors())
+	.options("*", cors())
 	.use(cors())
-	.use(favicon( path.join(app.get('public'), 'favicon.ico') ))
-	.use('/', serveStatic( app.get('public') ))
+	.use(favicon(path.join(app.get("public"), "favicon.ico")))
+	.use("/", serveStatic(app.get("public")))
 	.use(bodyParser.json())
 	.use(bodyParser.urlencoded({ extended: true }))
 	.configure(hooks())
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const app = require('./app');
-const port = app.get('port');
+const app = require("./app");
+const port = app.get("port");
 const server = app.listen(port);
 
-server.on('listening', () =>
-	console.log(`Feathers application started on ${app.get('host')}:${port}`)
+server.on("listening", () =>
+	console.log(`Feathers application started on ${app.get("host")}:${port}`)
 );
