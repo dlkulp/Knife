ca758ff795edd482c3146a257a0c46ad04c79090
Merfoo <fauzimerfoo@gmail.com>
2016-05-21 21:15:29 -0700

    Creates db, commented out auth stuff for testing

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,32 @@
 "use strict";
 const authentication = require("./authentication");
 const user = require("./user");
+const rethink = require("rethinkdbdash");
+const rethinkDB = require("feathers-rethinkdb");
 
 module.exports = function () {
 	const app = this;
 
 	app.configure(authentication);
 	app.configure(user);
+	
+	app.use("/user", user);
+	
+	// RethinkDB setup
+	var dbName = "feathers";
+	var tableName = "users";
+	
+	const r = rethink({
+		db: dbName
+	});
+	
+	// Create db if it doesn't exist
+	r.dbList().contains(dbName)
+		.do(dbExists => r.branch(dbExists, {create: 0}, r.dbCreate(dbName))).run()
+		
+		// Create table if doesn't exist
+		.then(() => {
+			return r.db(dbName).tableList().contains(tableName)
+			.do(tableExists => r.branch(tableExists, {created: 0}, r.tableCreate(tableName))).run()
+		});
 };
