f80960e67571fcd0085670f8c2658156841ca7cf
Graham Barber <grahammbarber@gmail.com>
2016-05-22 01:05:46 -0700

    Added vue routing, cleaned uneeded files

--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,31 @@
+import VueRouter from "vue-router";
 import Vue from "vue";
-import App from "./App.vue";
+import Home from "./components/Home.vue";
+import Browse from "./components/Browse.vue";
+// import App from "./App.vue";
+
+const App = Vue.extend({});
+
+Vue.use(VueRouter);
+var router = new VueRouter({
+	history: false
+});
+
+router.map({
+	"/": {
+		name: "root",
+		component: Home
+	},
+	"/browse": {
+		name: "browse",
+		component: Browse
+	}
+});
 
 /* eslint-disable no-new */
-new Vue({
-	el: "body",
-	components: { App }
-});
+// new Vue({
+// 	el: "body",
+// 	components: { App }
+// });
+
+router.start(App, "body");
