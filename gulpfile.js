071308dd50931f9999750043c00d1b27c8be6f9a
Graham Barber <grahammbarber@gmail.com>
2016-05-28 16:53:49 -0700

    Updated npm scripts, added front and backend dev commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-const gulp = require("gulp");
-const webpack = require("gulp-webpack");
 const args = require("yargs").argv;
+const gulp = require("gulp");
+const nodemon = require("gulp-nodemon");
+const shell = require("gulp-shell");
+const webpack = require("gulp-webpack");
 
 gulp.task("build", () => {
 	return gulp.src("./public/src/main.js")
 		.pipe(webpack(require("./webpack.config.js")))
 		.pipe(gulp.dest("./public/dist/js"));
 });
+
+gulp.task("dev:back", () => {
+	nodemon({
+		script: "./server.js"
+	});
+});
+
+gulp.task("dev:front", shell.task(["npm run dev:front"]));
